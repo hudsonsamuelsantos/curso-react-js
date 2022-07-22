7f399cbe3e9b5849ed9de035aa94e44763f70f83
Hudson Santos <hudsnss@gmail.com>
2022-07-21 21:54:37 -0300

    new lesson

---
+++ secao13-javascript-moderno/285-utilizando-let-e-const/scripts.js
@@ -0,0 +1,39 @@
+var x = 10
+var y = 15
+
+if (y > 10) {
+    var x = 5
+    console.log(x)
+}
+
+console.log(x)
+
+let a = 10
+let b = 15
+
+if (b > 10) {
+    let a = 5
+    console.log(a)
+}
+
+console.log(a)
+
+let i = 100
+
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+}
+
+console.log(i)
+
+const name = "Santos"
+
+function logName() {
+    const name = "Hudson"
+    console.log(name)
+}
+
+logName()
+
+console.log(name)
+
