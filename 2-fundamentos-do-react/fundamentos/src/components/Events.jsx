46d2f15401d381d1cfd0e9e1225668c80802a9c8
Hudson Santos <hudsnss@gmail.com>
2022-07-23 12:07:37 -0300

    new functions

--- 2-fundamentos-do-react/fundamentos/src/components/Events.jsx
+++ 2-fundamentos-do-react/fundamentos/src/components/Events.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 
 function Events() {
+
     const handleMyEvent = e => {
         console.log(e);
 
         console.log("Ativou evento")
     }
 
+    const renderSomething = x => {
+        if (x) {
+            return <p>Posso renderizar isso!</p>
+        } else {
+            return <p>Também posso renderizar isso!</p>
+        }
+    }
+
     return (
         <div>
             <div>
+                {/* Funções que não são bem pequenas devem ser separadas. Pode se seguir o padrão "handle"*/}
                 <button onClick={handleMyEvent} >Clique aqui</button>
+            </div>
+            <div>
+                {/* Se a função do evento for muito simples pode ser feita inline. */}
+                <button onClick={() => { console.log("Clicou!") }}>Cique aqui também!</button>
+            </div>
+            <div>
+                {renderSomething(true)}
+                {renderSomething(false)}
             </div>
         </div>
     )
