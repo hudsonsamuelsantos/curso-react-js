64ad5ed5726892632a348a9b3675f71d31cb566c
Hudson Santos <hudsnss@gmail.com>
2022-07-22 21:41:39 -0300

    new session

---
+++ 2-fundamentos-do-react/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FirstComponent = () => {
+    return (
+        <div className="teste">
+            <h1>Meu primeiro componente</h1>
+        </div>
+    )
+}
+
+export default FirstComponent
