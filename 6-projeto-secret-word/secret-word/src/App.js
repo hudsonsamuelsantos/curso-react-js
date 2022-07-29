4dad8857d70b6bd654fe257e4111c4bfb95aebc0
Hudson Santos <hudsnss@gmail.com>
2022-07-29 06:27:02 -0300

    new lesson

--- 6-projeto-secret-word/secret-word/src/App.js
+++ 6-projeto-secret-word/secret-word/src/App.js
@@ -17,9 +17,30 @@ function App() {
   const [stage, setStage] = useState(stages[0].name)
 
   const [words] = useState(wordsList)
-  console.log(words)
+
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+  const pickWordandCategory = () => {
+    const categories = Object.keys(words)
+    const category = 
+      categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    return { word, category }
+  }
 
   const startGame = () => {
+    const { word, category } = pickWordandCategory()
+    
+    let wordLetters = word.split("")
+    wordLetters = wordLetters.map(letter => letter.toLowerCase())
+
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(wordLetters)
     setStage(stages[1].name)
   }
 
