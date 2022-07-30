965f1caa08b131a7733b17775a2d381c09332251
Hudson Santos <hudsnss@gmail.com>
2022-07-30 19:36:01 -0300

    new components

--- 6-projeto-secret-word/secret-word/src/components/Game.js
+++ 6-projeto-secret-word/secret-word/src/components/Game.js
@@ -2,9 +2,25 @@ import './Game.css'
 
 function Game({ verifyLetter }) {
     return (
-        <div>
-            <h1>Game</h1>
-            <button onClick={verifyLetter} >Verificar letra</button>
+        <div className='game'>
+            <div className='points'>
+                <span>Pontuação: 000</span>
+            </div>
+            <h1>Descubra a palavra:</h1>
+            <h2 className='tip'>
+                Dica: <span>Dica...</span> 
+            </h2>
+            <div className='wordContainer'>
+                <span className='letter'>A</span>
+                <span className="blankSquare"></span>
+            </div>
+            <div className='letterCointainer'>
+                <p>Tente descobrir uma letra da palavra:</p>
+            </div>
+            <form>
+                <input type="text" name='letter' maxLength='1' required />
+                <button onClick={verifyLetter} >Jogar!</button>
+            </form>
         </div>
     )
 }
