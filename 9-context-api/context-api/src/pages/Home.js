a2381bf1dd2c55ba88190c1cb2ea2a4f4a12ff96
Hudson Santos <hudsnss@gmail.com>
2022-08-07 16:33:34 -0300

    new lesson

--- 9-context-api/context-api/src/pages/Home.js
+++ 9-context-api/context-api/src/pages/Home.js
@@ -1,10 +1,14 @@
-import { useContext } from "react"
+//import { useContext } from "react"
 import ChangeCounter from "../components/ChangeCounter"
-import { CounterContext } from "../context/CounterContext"
+import { useCounterContext } from "../hooks/useCounterContext"
+//import { CounterContext } from "../context/CounterContext"
 
 function Home() {
   
-  const { counter } = useContext(CounterContext)
+  //const { counter } = useContext(CounterContext)
+
+  // Utilização do Hook personalizado para o contexto 
+  const { counter } = useCounterContext()
 
   return (
     <div>
