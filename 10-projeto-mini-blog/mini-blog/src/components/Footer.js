fb4f9ceba30ea6a769c15dd657dede102ec66ef2
Hudson Santos <hudsnss@gmail.com>
2022-08-09 20:52:13 -0300

    css nav and footer

--- 10-projeto-mini-blog/mini-blog/src/components/Footer.js
+++ 10-projeto-mini-blog/mini-blog/src/components/Footer.js
@@ -2,7 +2,7 @@ import styles from './Footer.module.css'
 
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
         <h3>Escreva sobre o que você tem interresse!</h3>
         <p>Mini Blog &copy; 2022 | Hudson Santos - Desenvolvedor Front-End</p>
     </footer>
