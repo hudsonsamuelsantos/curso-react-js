3703dc34d23bd0f59cab993cfed527757655a6cb
Hudson Santos <hudsnss@gmail.com>
2022-08-10 06:24:55 -0300

    register page and form css

---
+++ 10-projeto-mini-blog/mini-blog/src/pages/Register/Register.js
@@ -0,0 +1,51 @@
+import styles from './Register.module.css'
+
+function Register() {
+  return (
+    <div className={styles.register}>
+      <h1>Cadastre-se para postar</h1>
+      <p>Crie sua conta e compartilhe suas histórias</p>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input 
+            type="text"
+            name='displayName'
+            required
+            placeholder='Insira seu nome de usuário' 
+          />
+        </label>
+        <label>
+          <span>E-mail:</span>
+          <input 
+            type='email'
+            name='email'
+            required
+            placeholder='Insira seu e-mail' 
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input 
+            type='password'
+            name='password'
+            required
+            placeholder='Insira sua senha' 
+          />
+        </label>
+        <label>
+          <span>Confirmar senha:</span>
+          <input 
+            type='password'
+            name='confirmPassword'
+            required
+            placeholder='Insira novamente sua senha' 
+          />
+        </label>
+        <button className='btn'>Cadastrar</button>
+      </form>
+    </div>
+  )
+}
+
+export default Register
