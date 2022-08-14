bd8cbaeecbc493d7d2499c8e9060e83902b3bf6d
Hudson Santos <hudsnss@gmail.com>
2022-08-13 22:55:55 -0300

    login page

--- 10-projeto-mini-blog/mini-blog/src/pages/Register/Register.js
+++ 10-projeto-mini-blog/mini-blog/src/pages/Register/Register.js
@@ -1,5 +1,4 @@
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useAuthentication } from '../../hooks/useAuthentication'
 import styles from './Register.module.css'
 
