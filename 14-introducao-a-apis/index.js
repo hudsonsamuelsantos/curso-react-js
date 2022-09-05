783270d23f6564c4319f088f437da1ab5906a16c
Hudson Santos <hudsnss@gmail.com>
2022-09-05 20:21:34 -0300

    create api routes

--- 14-introducao-a-apis/index.js
+++ 14-introducao-a-apis/index.js
@@ -10,8 +10,22 @@ app.use(
 app.use(express.json())
 
 //rotas - endpoints
+app.post('/createproduct', (req, res) => {
+    const name = req.body.name
+    const price = req.body.price
+
+    if (!name) {
+        res.status(422).json({ message: 'O campo é obrigatório!' })
+        return
+    }
+
+    console.log(name, price)
+
+    res.status(201).json({ message: `O produto ${name} foi criado com sucesso!` })
+})
+
 app.get('/', (req, res) => {
-    res.json({ message: 'Primeira rota criada com sucesso!' })
+    res.status(200).json({ message: 'Primeira rota criada com sucesso!' })
 })
 
 app.listen(3000)
