d69727f553281a7aa0caffc28dfa90a9927b1024
Hudson Santos <hudsnss@gmail.com>
2022-09-06 06:07:58 -0300

    useState and inputs review

--- 11-hooks/reacthooks/src/components/HookUseState.js
+++ 11-hooks/reacthooks/src/components/HookUseState.js
@@ -5,10 +5,17 @@ const HookUseState = () => {
     let name = 'João'
 
     const [stateName, setStateName] = useState('Hudson')
+    const [age, setAge] = useState('?')
 
     const changeName = () => {
         name = 'Santos'
         setStateName('Santos')
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        console.log(age)
     }
 
     return (
@@ -17,6 +24,12 @@ const HookUseState = () => {
             <p>Variável: {name}</p>
             <p>Estado: {stateName}</p>
             <button onClick={changeName}>Mudar nome</button>
+            <p>Digite sua idade:</p>
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={e => setAge(e.target.value)} value={age} />
+                <input type="submit" value="Enviar" />
+            </form>
+            <p>Sua idade é : {age}</p>
         </div>
     )
 }
