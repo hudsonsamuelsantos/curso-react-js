930fe3112473c1667b1e89bc2ec44686601779e4
Hudson Santos <hudsnss@gmail.com>
2022-09-07 06:11:03 -0300

    useEffect with deps array (review)

--- 11-hooks/reacthooks/src/components/HookUseEffect.js
+++ 11-hooks/reacthooks/src/components/HookUseEffect.js
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from 'react'
 
 const HookUseEffect = () => {
 
+    const [number, setNumber] = useState(1)
+    const [anotherNumber, setAnotherNumber] = useState(0)
+
     //sem dependencias ele executa quando o componente é renderizado
     useEffect(() => console.log('Executei!'))
 
     //com dependencias vazias executa só uma vez, quando o componente é renderizado
     useEffect(() => console.log('Executei apenas uma vez!'), [])
 
-    const [number, setNumber] = useState(1)
+    //com dependencias, executa sempre que o valor da dependencia for alterado
+    useEffect(() => console.log('Another number foi alterado!'), [anotherNumber])
 
     const increment = () => {
         setNumber(number + 1)
@@ -18,8 +22,10 @@ const HookUseEffect = () => {
         <div>
             <hr />
             <h2>useEffect</h2>
-            <p>Número: {number}</p>
+            <p>Number: {number}</p>
             <button onClick={increment}>+1</button>
+            <p>Another number: {anotherNumber}</p>
+            <button onClick={() => setAnotherNumber(anotherNumber + 1)}>+1</button>
         </div>
     )
 }
