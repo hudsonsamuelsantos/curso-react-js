7a1226a089b38e31e7fa1592ab505929c519d14f
Hudson Santos <hudsnss@gmail.com>
2022-09-08 19:27:48 -0300

    useRef with DOM (review)

--- 11-hooks/reacthooks/src/components/HookUseRef.js
+++ 11-hooks/reacthooks/src/components/HookUseRef.js
@@ -6,9 +6,20 @@ const HookUseRef = () => {
     const [counter1, setCounter1] = useState(0)
     const [counter2, setCounter2] = useState(0)
 
+    const inputRef = useRef()
+    const [text, setText] = useState('')
+
     useEffect(() => {
         numberRef.current = numberRef.current + 1
     })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        setText('')
+
+        inputRef.current.focus()
+    }
 
     return (
         <div>
@@ -19,6 +30,10 @@ const HookUseRef = () => {
             <button onClick={() => setCounter1(counter1 + 1)}>Contador A</button>
             <p>Counter 2: {counter2}</p>
             <button onClick={() => setCounter2(counter2 + 1)}> Contador B</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" ref={inputRef} value={text} onChange={(e) => setText(e.target.value)} />
+                <input type="submit" value='Enviar' />
+            </form>
         </div >
     )
 }
