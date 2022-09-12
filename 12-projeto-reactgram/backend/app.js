33a8f2b1ff45afd4d9ed32a2357389ac58538d88
Hudson Santos <hudsnss@gmail.com>
2022-09-12 20:55:51 -0300

    middlewares

--- 12-projeto-reactgram/backend/app.js
+++ 12-projeto-reactgram/backend/app.js
@@ -15,6 +15,17 @@ app.use(express.urlencoded({ extended: false }))
 // routes
 const router = require("./routes/Router.js")
 
+// solve CORS
+// soluciona o problema de várias reqs no mesmo domínio
+// origin se refere ao endereço do projeto front-end
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
+
+// upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+// db connection
+require("./config/db.js")
+
 app.use(router)
 
 app.listen(port, () => {
