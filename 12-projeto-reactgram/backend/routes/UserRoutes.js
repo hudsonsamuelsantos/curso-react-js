b02b7484a2c4dd2494959e8cb5e732b48828799a
Hudson Santos <hudsnss@gmail.com>
2022-09-16 06:15:28 -0300

    login validation

--- 12-projeto-reactgram/backend/routes/UserRoutes.js
+++ 12-projeto-reactgram/backend/routes/UserRoutes.js
@@ -2,14 +2,18 @@ const express = require("express")
 const router = express()
 
 // controller
-const { register } = require("../controllers/UserController")
+const { register, login } = require("../controllers/UserController")
 
 // middlewares
 // algo que executa no meio de uma req 
 const validate = require("../middlewares/handleValidation")
-const { userCreateValidation } = require("../middlewares/userValidations")
+const {
+    userCreateValidation,
+    loginValidation
+} = require("../middlewares/userValidations")
 
 // routes
 router.post("/register", userCreateValidation(), validate, register)
+router.post("/login", loginValidation(), validate, login)
 
 module.exports = router
