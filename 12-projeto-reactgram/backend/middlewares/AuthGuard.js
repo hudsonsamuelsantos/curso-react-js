390744d98a68dabd6599875a7a5b66717327a40e
Hudson Santos <hudsnss@gmail.com>
2022-09-17 21:33:41 -0300

    get current user

--- 12-projeto-reactgram/backend/middlewares/AuthGuard.js
+++ 12-projeto-reactgram/backend/middlewares/AuthGuard.js
@@ -4,7 +4,7 @@ const jwtSecret = process.env.JWT_SECRET
 
 const authGuard = async (req, res, next) => {
     const authHeader = req.headers["authorization"]
-    const token = authHeader && authHeader.split("")[1]
+    const token = authHeader && authHeader.split(" ")[1]
 
     // check if header
     if (!token) return res.status(401).json({ errors: ["Acesso negado!"] })
@@ -14,6 +14,8 @@ const authGuard = async (req, res, next) => {
         const verified = jwt.verify(token, jwtSecret)
 
         req.user = await User.findById(verified.id).select("-password")
+
+        next()
     } catch (error) {
         res.status(401).json({ errors: ["Token inválido"] })
     }
--- 12-projeto-reactgram/backend/routes/UserRoutes.js
+++ 12-projeto-reactgram/backend/routes/UserRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express()
 
 // controller
-const { register, login } = require("../controllers/UserController")
+const { register, login, getCurrentUser } = require("../controllers/UserController")
 
 // middlewares
 // algo que executa no meio de uma req 
@@ -11,9 +11,11 @@ const {
     userCreateValidation,
     loginValidation
 } = require("../middlewares/userValidations")
+const authGuard = require("../middlewares/AuthGuard")
 
 // routes
 router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
+router.get("/profile", authGuard, getCurrentUser)
 
 module.exports = router
