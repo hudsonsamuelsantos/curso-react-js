4c69c8adb11ed5c6e2718b36e9fdd434f05c2cb6
Hudson Santos <hudsnss@gmail.com>
2022-09-18 23:14:54 -0300

    user update middleware

--- 12-projeto-reactgram/backend/routes/UserRoutes.js
+++ 12-projeto-reactgram/backend/routes/UserRoutes.js
@@ -2,20 +2,23 @@ const express = require("express")
 const router = express()
 
 // controller
-const { register, login, getCurrentUser } = require("../controllers/UserController")
+const { register, login, getCurrentUser, update } = require("../controllers/UserController")
 
 // middlewares
 // algo que executa no meio de uma req 
 const validate = require("../middlewares/handleValidation")
 const {
     userCreateValidation,
-    loginValidation
+    loginValidation,
+    userUpdateValidation
 } = require("../middlewares/userValidations")
 const authGuard = require("../middlewares/AuthGuard")
+const { imageUpload } = require("../middlewares/imageUpload")
 
 // routes
 router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
 router.get("/profile", authGuard, getCurrentUser)
+router.put("/", authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"), update)
 
 module.exports = router
