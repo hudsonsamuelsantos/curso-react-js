ca8562377e289af0a0da20766496a47f528d0c33
Hudson Santos <hudsnss@gmail.com>
2022-09-25 07:46:52 -0300

    router config

--- 12-projeto-reactgram/frontend/src/App.js
+++ 12-projeto-reactgram/frontend/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+// router
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './components/Home/Home'
+import Register from './components/Auth/Register'
+import Login from './components/Auth/Login'
+
+
 function App() {
   return (
     <div className="App">
-      <h1>ReactGram</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
