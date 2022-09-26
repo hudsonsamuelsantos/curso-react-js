2c1027dbb953a56b472459a62c9b4ba7dafbb3d7
Hudson Santos <hudsnss@gmail.com>
2022-09-26 06:20:22 -0300

    add navbar and footer

--- 12-projeto-reactgram/frontend/src/App.js
+++ 12-projeto-reactgram/frontend/src/App.js
@@ -1,21 +1,30 @@
+// styles
 import './App.css';
 
 // router
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './components/Home/Home'
-import Register from './components/Auth/Register'
-import Login from './components/Auth/Login'
+
+// pages
+import Home from './pages/Home/Home'
+import Register from './pages/Auth/Register'
+import Login from './pages/Auth/Login'
+
+// components
+import NavBar from './components/NavBar/NavBar'
+import Footer from './components/Footer/Footer';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
