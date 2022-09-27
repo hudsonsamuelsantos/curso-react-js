ac26ef8e96d8aff490df622d8aafa68248345cea
Hudson Santos <hudsnss@gmail.com>
2022-09-27 06:23:44 -0300

    register page

--- 12-projeto-reactgram/frontend/src/pages/Auth/Register.js
+++ 12-projeto-reactgram/frontend/src/pages/Auth/Register.js
@@ -1,8 +1,32 @@
+// styles
 import "./Auth.css"
 
+// components
+import { Link } from 'react-router-dom'
+
+// hooks
+import { useEffect, useState } from 'react'
+
 const Register = () => {
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
+
     return (
-        <div>Register</div>
+        <div>
+            <h2>ReactGram</h2>
+            <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Nome" />
+                <input type="email" placeholder="E-mail" />
+                <input type="password" placeholder="Senha" />
+                <input type="password" placeholder="Confirme sua senha" />
+                <input type="submit" value="Cadastrar" />
+            </form>
+            <p>
+                Já tem uma conta? <Link to='/login' >Clique aqui.</Link>
+            </p>
+        </div>
     )
 }
 
