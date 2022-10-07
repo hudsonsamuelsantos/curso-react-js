cd727e094a140502d230912574ba286bdd52a0a5
Hudson Santos <hudsnss@gmail.com>
2022-10-07 06:20:38 -0300

    login action

--- 12-projeto-reactgram/frontend/src/slices/authSlice.js
+++ 12-projeto-reactgram/frontend/src/slices/authSlice.js
@@ -25,6 +25,21 @@ export const register = createAsyncThunk(
     }
 )
 
+// sign in anuser
+export const login = createAsyncThunk(
+    'auth/login',
+    async (user, thunkAPI) => {
+        const data = await authService.login(user)
+
+        // check for errors
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data
+    }
+)
+
 export const logout = createAsyncThunk(
     'auth/logout',
     async () => {
@@ -59,6 +74,18 @@ export const authSlice = createSlice({
             state.loading = false
             state.success = true
             state.error = null
+            state.user = null
+        }).addCase(login.pending, state => {
+            state.loading = true
+            state.error = false
+        }).addCase(login.fulfilled, (state, action) => {
+            state.loading = false
+            state.success = true
+            state.error = null
+            state.user = action.payload
+        }).addCase(login.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.payload
             state.user = null
         })
     }
