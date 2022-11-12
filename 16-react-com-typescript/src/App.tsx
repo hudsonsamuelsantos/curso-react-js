38e12b4a1f4e5d8f2ac14a159a6143a0bc7a0550
Hudson Santos <hudsnss@gmail.com>
2022-11-11 21:09:41 -0300

    new lesson: vars and functions (typescript)

--- 16-react-com-typescript/src/App.tsx
+++ 16-react-com-typescript/src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
 import './App.css';
 
 function App() {
+
+  const name: string = "Hudson"
+  const age: number = 23
+  const isWorking: boolean = true
+
+  const userGreeting = (name: string): string => {
+    return `Olá ${name}!`
+  }
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
+      <p>Nome: {name}</p>
+      <p>Idade: {age}</p>
+      {isWorking && <p>Está trabalhando...</p>}
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
