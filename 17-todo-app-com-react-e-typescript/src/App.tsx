641497091774db9d596494bb6dee69f25393b052
Hudson Santos <hudsnss@gmail.com>
2022-11-16 06:09:00 -0300

    header and footer (react + typescript)

--- 17-todo-app-com-react-e-typescript/src/App.tsx
+++ 17-todo-app-com-react-e-typescript/src/App.tsx
@@ -1,6 +1,15 @@
+import { Header } from "./components/header"
+import { Footer } from "./components/footer"
+
 function App() {
   return (
-    <div>todo app</div>
+    <div>
+      <Header />
+      <main>
+        conteudo
+      </main>
+      <Footer />
+    </div>
   );
 }
 
